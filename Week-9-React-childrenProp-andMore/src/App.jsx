5697c2bf592c13ebcc797423c3d9a9a71ed74bf8
Tushar Sharma <onlyforcode23@gmail.com>
2025-11-20 09:50:21 +0000

    fix import issue in app

--- Week-9-React-childrenProp-andMore/src/App.jsx
+++ Week-9-React-childrenProp-andMore/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card_Component from '../Components/Card_Children.jsx'
+import Card_Component from '../Components/Card_Children'
 
 function App() {
   const [count, setCount] = useState(0)
