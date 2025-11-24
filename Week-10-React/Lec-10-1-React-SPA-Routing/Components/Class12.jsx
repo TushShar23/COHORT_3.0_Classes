78a11a73712d1670154f104ae21982852ebc6d4a
Tushar Sharma <onlyforcode23@gmail.com>
2025-11-24 09:41:46 +0000

    Navigation using <Link> and useNavigate() hook with different usage purpose

--- Week-10-React/Lec-10-1-React-SPA-Routing/Components/Class12.jsx
+++ Week-10-React/Lec-10-1-React-SPA-Routing/Components/Class12.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom'
 
 function Class12(){
+    // Returns a function that lets you navigate programmatically in the browser in response to user interactions or effects.
+
+    const navigate = useNavigate();
+    
+    function redirectUser(){
+        navigate("/")
+    }
+
     return (
-        <div>
+        <div style={{fontSize:"22px"}}>
             Class 12 section
+            <br />
+            <button onClick={redirectUser} style={{fontSize:"18px",marginTop:"1rem",marginLeft:"0.3rem",backgroundColor:"wheat",padding:"0.3rem",borderRadius:"0.5rem"}}>Back to home page</button>
         </div>
     )
 }
 
 export default Class12;
+
+// let say after coming to class 12 section USER WANTS TO GO BACK WE NEED TO NAVIGATE USER TO THE HOME PAGE.
+// This can be done using "useEffect()" but this is mostly used when we automatically wants to REDIRECT USER TO THE HOME PAGE.LIKE form submission,login etc..
+
+// Here we are using "useNavigate()" from "react-router-dom" for navigating user BACK TO HOME
--- Week-10-React/Lec-10-1-React-SPA-Routing/src/App.jsx
+++ Week-10-React/Lec-10-1-React-SPA-Routing/src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
 import LandingPage from '../Components/LandingPage';
 import Class11 from '../Components/Class11';
 import Class12 from '../Components/Class12';
 import NoContentFound from '../Components/ErrorPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div style={{backgroundColor:"black",height:"90vh",color:"white"}}>
      <BrowserRouter>
+        <Link to={"/"} style={{color:"yellow",fontSize:"30px"}}>ALLEN</Link>|
+        <Link to={"/neet/online-coaching-class-11"} style={{color:"yellow",fontSize:"30px"}}>Class 11</Link>|
+        <Link to={"/neet/online-coaching-class-12"} style={{color:"yellow",fontSize:"30px"}}>Class 12</Link>| 
+
+        {/*  we have LINK component/TAG from "REACT ROUTER DOM" for navigation.<a> anchor tag cannot be used here coz if we use a tag then there is no sense of SPA coz every time we navigate to a new page.It RELOADS THE WHOLE CONTENT.THATSWHY <LINK> is USED.<a> again and again RELOADS THE WHOLE INDEX.JS on the page */}
         <Routes>
           <Route path="/" element={ <LandingPage/> }></Route>
           <Route path="/neet/online-coaching-class-11" element={ <Class11/> }></Route>
@@ -35,6 +39,14 @@ React Router DOM is a library specifically designed for handling routing in Reac
 Library is like a cool dude because it has restrictions BUT NOT AS MUCH IN A FRAMEWORK
 
 
-so far we have done a simple routing in react.WE HAVEN'T DONE NAVIGATION USING (a AND link component).TO BE PRECISE WE USE "link component" FOR MAKING OUR APPLICATION a true SINGLE PAGE APPLICATION.
+so far we have done a simple routing in react.WE HAVEN'T DONE NAVIGATION USING (a AND link component).TO BE PRECISE WE USE "link component/Tag" FOR MAKING OUR APPLICATION a true SINGLE PAGE APPLICATION.because <a> reloads the whole index.js file again and again
+
+<LINK> will not RELOAD THE WHOLE PAGE AGAIN AND AGAIN.
+
+
+> There are 2 ways to NAVIGATE A USER:
+1. thru "<LINK> "- use this when user manually will CLICK A BUTTON
+2." useNavigate()" ( hook) - use this when you automatically wants to take user back to a page.
+ex - after SIGNING UP redirect user automatically to the HOME PAGE/USER INFO PAGE.Like this doesn't require users action.
 
 */
