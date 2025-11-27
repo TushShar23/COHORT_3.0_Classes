1a957960f0b989c6643d387a1d3d2370889db7f5
Tushar Sharma <onlyforcode23@gmail.com>
2025-11-27 12:37:27 +0000

    fix bug in app

--- Week-10-React/Lec-10-2-React-RollingState-ContextAPI/src/App.jsx
+++ Week-10-React/Lec-10-2-React-RollingState-ContextAPI/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
 
 // let say you have a component in app() which is <lightbulb> which uses two more components <bulbState> and <TogglebulbState> and these components Uses COMMON STATE.IF WE DEFINE THAT STATE IN ANY OF THESE COMPONENT THE OTHER ONE WILL NOT BE ABLE TO ACCESS IT BECAUSE OF SCOPE SO FOR THAT PURPOSE WE DECLARE THAT STATE VARIABLE IN THE app() WHICH IS THE LOWEST COMMON ANCESTOR OF BOTH THE COMPONENTS.
 
+// ROLLING UP STATE is an UNOPTIMAL WAY FOR MANAGING STATE.THIS WE HAVE TO DO COZ IN MOST OF THE CASES COMPONENTS WILL SHARE COMMON PROPERTIES.BUT THIS IS NOT THE BEST WAY .
 /*
 
 As your application grows, you might find that multiple components need access to the same state. Instead of duplicating state in each component, you can lift the state up to the LCA, allowing the common ancestor to manage it.
