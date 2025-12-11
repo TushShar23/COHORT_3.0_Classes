35adb40be3152ce1f14bea0fe4362c362e5ac313
Tushar Sharma <onlyforcode23@gmail.com>
2025-12-11 09:20:40 +0000

    implement useDebounce hook

---
+++ Week-11-React-Contd/Lec-1-useDebounce-hook-v1/Components/Input_debounce.jsx
@@ -0,0 +1,9 @@
+function input_val(changeFunc){
+    return(
+        <div>
+            <input type="text" name="myText" id="" onChange={changeFunc}/>
+        </div>
+    )
+}
+
+export default input_val;
---
+++ Week-11-React-Contd/Lec-1-useDebounce-hook-v1/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import InputBox from '../Components/Input_debounce'
+
+function App() {
+  return(
+    <div>
+      <InputBox />
+    </div>
+  )
+}
+
+
+/*
+
+
+
+
+*/
+
+export default App
