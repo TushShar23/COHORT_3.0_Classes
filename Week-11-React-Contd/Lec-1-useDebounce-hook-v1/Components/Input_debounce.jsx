36636e790d9f3137a6c6d7e7cd78dd12dbe79e26
Tushar Sharma <onlyforcode23@gmail.com>
2025-12-11 16:53:10 +0000

    remove input component and add main function component

--- Week-11-React-Contd/Lec-1-useDebounce-hook-v1/Components/Input_debounce.jsx
+++
@@ -1,9 +0,0 @@
-function input_val(changeFunc){
-    return(
-        <div>
-            <input type="text" name="myText" id="" onChange={changeFunc}/>
-        </div>
-    )
-}
-
-export default input_val;
--- Week-11-React-Contd/Lec-1-useDebounce-hook-v1/src/App.jsx
+++ Week-11-React-Contd/Lec-1-useDebounce-hook-v1/src/App.jsx
@@ -1,19 +1,27 @@
 import { useState } from 'react'
 import './App.css'
-import InputBox from '../Components/Input_debounce'
+import useDebounce from '../Components/Debounce'
+import MainFunc from '../Components/MainFunc'
 
 function App() {
+  const debouncefn = useDebounce(MainFunc)
   return(
     <div>
-      <InputBox />
+      <input type="text" name="myText" id="textarea" onChange={debouncefn}/>
     </div>
   )
 }
 
 
+
+
 /*
 
+// YES IT IS WORKING.IF USER TYPING TIME GAP IS 200MS THEN IT WILL AUTOMATICALLY CALL THE MAINCALLING FUNCTION.
+(matlab request tab jaayegi jab user apne keyboard se dur ho jaayega aur 200ms pass ho jaayenge nahi toh nhi jaayegi)
+Consider a scenario where a user is typing into a search input field to find specific items on a website. Without optimization, every keystroke would trigger an API call to fetch search results. This means that even before the user finishes typing a word, multiple unnecessary API requests are sent, potentially overwhelming the server and slowing down the application.
 
+By utilizing the useDebounce hook, we can delay the execution of the API call until the user has finished typing, thus reducing the number of unnecessary requests and optimizing the application's performance.
 
 
 */
