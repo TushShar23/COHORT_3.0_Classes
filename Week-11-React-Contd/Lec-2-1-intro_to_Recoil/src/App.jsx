d18dffdbfde6e9d8be747bf7fa9323dcc1e431c1
Tushar Sharma <onlyforcode23@gmail.com>
2025-12-23 18:03:50 +0000

    add comment for the context api demonstration

--- Week-11-React-Contd/Lec-2-1-intro_to_Recoil/src/App.jsx
+++ Week-11-React-Contd/Lec-2-1-intro_to_Recoil/src/App.jsx
@@ -22,5 +22,6 @@ function App() {
 
 export default App
 
+// This is a demonstration of USING STATE MANAGEMENT using context API which is not an optimal approach.
 
 // no of re-renders are not optimised when we used contextAPI.All components are re-rendering again and again EVEN IF I CHANGE ONLY 1 COMPONENT.This is not an optimised way to create a counter app.MAINLY STATE MANAGEMENT AND OPTIMISATIONS AT A GOOD LEVEL ARE REQUIRED WHEN WE ARE MAKING HIGH FPS APPS like games,real time apps like figma,canva and all.MAINLY ONLY {COUNTER} COMPONENT SHOULD RE-RENDER COZ IT CONTAINS THE COUNT WHICH IS CHANGING TIME TO TIME.
