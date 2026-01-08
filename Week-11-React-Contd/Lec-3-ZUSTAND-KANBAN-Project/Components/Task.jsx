61ca38c581a2cccf280717531e6b57ab55289267
Tushar Sharma <onlyforcode23@gmail.com>
2026-01-08 18:29:06 +0000

    add method to manage opacity of task on dragstart

--- Week-11-React-Contd/Lec-3-ZUSTAND-KANBAN-Project/Components/Task.jsx
+++ Week-11-React-Contd/Lec-3-ZUSTAND-KANBAN-Project/Components/Task.jsx
@@ -13,8 +13,12 @@ export default function Tasks({title}){
     const mytasks = useTaskStore((store)=>store.tasks.find(t=>t.title===title),shallow)
     const deleteTask = useTaskStore((store)=>store.deleteTask)
 
+    function handleDragStart(e){
+        e.target.style.opacity = "0.5"
+    }
+
     return(
-        <div className='tasks' draggable>
+        <div className='tasks' draggable onDragStart={handleDragStart}>
             <div>{title}</div>
             <div className='bottom-wrapper'>
                 <div className='del-icon'><img src="../src/assets/Delicon.png" alt="icon" onClick={()=>{deleteTask(title)}}/></div>
