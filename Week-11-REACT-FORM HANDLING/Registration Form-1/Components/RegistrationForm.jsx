77758d87c6b628c11d32b821227b65f4d8034ddb
Tushar Sharma <onlyforcode23@gmail.com>
2026-01-20 18:44:05 +0000

    define states using useState(this form is the wrong way to handle forms in react but one can do this too

--- Week-11-REACT-FORM HANDLING/Registration Form-1/Components/RegistrationForm.jsx
+++ Week-11-REACT-FORM HANDLING/Registration Form-1/Components/RegistrationForm.jsx
@@ -1,13 +1,60 @@
+import { useState } from "react";
 import "../src/Form.css"
 
 
 const RegistrationForm = ()=>{
-
+    const [fname,setFname] = useState("")
+    const [lname,setLname] = useState("")
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const [phone,setPhone] = useState(null)
 
     const handleSubmit = (e)=>{
         e.preventDefault();
     }
 
+    const handleInputData = (event)=>{
+        const name = event.target.name // this is the name property of the input box.we know the event.target will be an input box and we are catching the data from it.
+        const value = event.target.value
+
+        switch (name) {
+            case "FirstName":
+                setFname(value)
+                break;
+            // value is the attribute of the input box which contains the state variable fname's value.
+            case "LastName":
+                setLname(value);
+                break;
+
+            case "email":
+                setEmail(value);
+                break;
+
+            case "password":
+                setPassword(value);
+                break;
+
+            case "phone":
+                setPhone(value);
+                break;
+        
+            default:
+                break;
+        }
+
+    }
+
+    const showData = ()=>{
+        const dataObj = {
+            firstname:fname,
+            lastname:lname,
+            email:email,
+            password:password,
+            phone:phone
+        }
+
+        console.log(dataObj)
+    }
 
 
     return(
@@ -20,39 +67,39 @@ const RegistrationForm = ()=>{
                     <label htmlFor="FirstName">
                         <strong>First Name</strong>
                     </label>
-                    <input type="text" name="FirstName" id="fname" placeholder="Enter your first name"/>
+                    <input type="text" name="FirstName" id="fname" placeholder="Enter your first name" onChange={(e)=>handleInputData(e)} value={fname}/>
                 </section>
 
                 <section className="lname">
                     <label htmlFor="LastName">
                         <strong>Last Name</strong>
                     </label>
-                    <input type="text" name="LastName" id="lname" placeholder="Enter your last name"/>
+                    <input type="text" name="LastName" id="lname" placeholder="Enter your last name" onChange={(e)=>handleInputData(e)} value={lname}/>
                 </section>
 
                 <section className="email">
                     <label htmlFor="email">
                         <strong>Email</strong>
                     </label>
-                    <input type="text" name="email" id="myemail" placeholder="Enter your email address"/>
+                    <input type="text" name="email" id="myemail" placeholder="Enter your email address" onChange={(e)=>handleInputData(e)} value={email}/>
                 </section>
 
                 <section className="Password">
                     <label htmlFor="password">
                         <strong>Password</strong>
                     </label>
-                    <input type="text" name="password" id="mypassword" placeholder="Enter your password"/>
+                    <input type="text" name="password" id="mypassword" placeholder="Enter your password" onChange={(e)=>handleInputData(e)} value={password}/>
                 </section>
 
                 <section className="Phone">
                     <label htmlFor="phone">
                         <strong>Phone Number</strong>
                     </label>
-                    <input type="text" name="phone" id="myphone" placeholder="Enter your phone number"/>
+                    <input type="text" name="phone" id="myphone" placeholder="Enter your phone number" onChange={(e)=>handleInputData(e)} value={phone}/>
                 </section>
                 
                 <section className="buttons">
-                    <button>Sign Up</button>
+                    <button onClick={showData}>Sign Up</button>
                 </section>
             </form>
         </div>
