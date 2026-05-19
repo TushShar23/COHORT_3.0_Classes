37f04f5829fa07cc8a07a899edf6bb9486f8458f
Tushar Sharma <onlyforcode23@gmail.com>
2026-05-19 11:46:29 +0100

    Toggle button feature using usestate hook and tailwind css

--- Week-13-Tailwind/Lec-2-Tailwind/src/Components/Sidebar.jsx
+++ Week-13-Tailwind/Lec-2-Tailwind/src/Components/Sidebar.jsx
@@ -1,38 +1,45 @@
+import { useState } from "react"
 
 export const SideBar = ()=>{
+    const [open,setOpen] = useState(false)
+
+    // const openBar = ()=>{
+
+    // }
     return(
         <div className="Side-div flex text-white justify-center items-end h-screen p-4">
             <div className="tog-btn flex justify-center items-center">
-             <i class="ri-menu-line text-white text-xl absolute top-10 left-5"></i>
+             <i class={`ri-menu-line text-white text-xl font-bold absolute top-10 left-5 ${open && "hidden"}`} onClick={()=>{setOpen(true)}}></i>
             </div>
             {/* left box */}
             
-            <div className="div-left h-full bg-white w-0 md:w-75 md:flex md:flex-col justify-start items-start text-black p-3 gap-10 transition-all duration-300 ease-in-out md:translate-x-0 -translate-x-96 ">
+            <div className={`div-left h-full bg-white w-0 md:w-75 md:flex  gap-10 md:flex-col justify-start items-start text-black p-3  transition-all duration-300 ease-in-out md:translate-x-0 -translate-x-96 ${open && "w-75 translate-x-0 flex flex-col gap-10"}`}>
                 {/* what i have done here to make the sidebar is that applied a transition slide from left when the screen is greater or equal to md then it appears its width is 75 else(mobile screen) w-0  and the right div takes the whole space when mobile screen BY DEFAULT WIDTH OF THE SIDEBAR IS 0 */}
-                <div className="logo">
+                <div className="logo flex gap-25 items-center justify-center p-2">
                     <div className="name bg-blue-950 px-3 py-2 rounded-lg">
                         <p className="text-white"><i class="ri-robot-2-fill mr-2"></i>Webinar<span className="text-gray-500">.gg</span></p>
                     </div>
+                    <i class="ri-close-line text-2xl font-bold md:hidden" onClick={()=>{setOpen(false)}}></i>
                 </div>
 
                 <div className="menu flex flex-col gap-3 justify-center items-center">
-                    <div className="w-58 flex justify-between items-center p-1">
+                    <div className="w-58 flex justify-between items-center p-1 ml-2">
                         <span>Home</span>
                         <i class="ri-home-3-fill"></i>
                     </div>
-                    <div className="w-58 flex justify-between items-center p-1">
+                    <div className="w-58 flex justify-between items-center p-1 ml-2">
                         <span>Webinars</span>
                         <i class="ri-team-line"></i>
                     </div>
-                    <div className=" w-58 flex justify-between items-center p-1">
+                    <div className=" w-58 flex justify-between items-center p-1 ml-2">
                         <span>Billings</span>
                         <i class="ri-wallet-line"></i>
                     </div>
-                    <div className=" w-58 flex justify-between items-center p-1">
+                    <div className=" w-58 flex justify-between items-center p-1 ml-2">
                         <span>User Management</span>
                         <i class="ri-user-fill"></i>
                     </div>
-                    <div className=" w-58 flex justify-between items-center p-1">
+                    <div className=" w-58 flex justify-between items-center p-1 ml-2">
                         <span>Settings</span>
                         <i class="ri-settings-5-line"></i>
                     </div>
