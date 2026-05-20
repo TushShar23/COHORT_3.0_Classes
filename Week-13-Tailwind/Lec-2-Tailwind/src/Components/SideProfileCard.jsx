c8b34d747fe5eeb5f131bb8357b596f434575f1b
Tushar Sharma <onlyforcode23@gmail.com>
2026-05-20 08:39:50 +0100

    Complete side profile card component

---
+++ Week-13-Tailwind/Lec-2-Tailwind/src/Components/SideProfileCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SideProfileCard = ({img,name,id,phno,location}) => {
+  return (
+    <div className='h-84 w-60 shadow-lg ml-20 absolute top-30 bg-white flex flex-col items-center rounded-2xl gap-4'>
+        <div className="profile w-28 flex justify-center items-center p-2 mt-8">
+            <img src={img} alt="Avatar Image" className='rounded-2xl'/>
+        </div>
+        <div className='info text-lg text-gray-500 flex flex-col items-center gap-3'>
+            <h1 className='text-black'><strong>{name}</strong></h1>
+            <div className='creds text-[16px] flex flex-col items-center'>
+                <p>{id}</p>
+                <p>{phno}</p>
+            </div>
+        <p className='text-[16px]'>{location}</p>
+        </div>
+    </div>
+  )
+}
+
+export default SideProfileCard
