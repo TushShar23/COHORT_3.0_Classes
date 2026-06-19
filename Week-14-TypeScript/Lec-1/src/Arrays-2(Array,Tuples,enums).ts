dd41a1586a93787e695b67701897387318487278
Tushar Sharma <onlyforcode23@gmail.com>
2026-06-19 21:13:58 +0100

    more about tuples and enums

--- Week-14-TypeScript/Lec-1/src/Arrays-2(Array,Tuples,enums).ts
+++ Week-14-TypeScript/Lec-1/src/Arrays-2(Array,Tuples,enums).ts
@@ -120,6 +120,89 @@ console.log(ChaiItems);
 // console.log(ChaiItems[2]); In memory there is nothing like ("Green" and 25)
 
 
+// ENUMS 
+// Enums(enumeration) is way to DEFINE A FIXED SET OF NAMED CONSTANTS.An enum is a type that represents a fixed set of named constants. It improves readability and prevents invalid values by restricting variables to predefined options.
+
+enum CupSize{
+    SMALL,
+    LARGE,
+    XLARGE
+}
+
+const size = CupSize.SMALL;
+console.log(size); // THIS IS SHOWING THE INDEX of the value.
+console.log(CupSize);
+
+// PROPERTY : When we give a initial/Default value(NUMBER) to a constant then the following named constants WILL BE AUTO-INCREMENTED."Mtlb agar 100 dedi value toh niche walo mei 101,102,103 and so on.. values aa jayengi"
+
+enum NumVals{
+    FIRST = 100,
+    SECOND,
+    THIRD,
+    FOURTH
+}
+
+let numVal = NumVals.SECOND
+console.log(numVal);
+console.log(NumVals);
+
+// EXAMPLE OF STATUS
+
+enum Status{
+    SUCCESS = "Success",
+    PENDING = "Pending",
+    FAILED  = "Failed"
+}
+
+let RespStatus: Status = Status.SUCCESS;
+console.log(RespStatus);
+console.log(Status);
+
+
+// THE MAIN REASON BEHIND USING ENUMS is that they restricting values to a predefined set Unlike JS/TS objects.MAINLY THEY ARE USED WHERE THERE IS HEAVY OOPS STRUCTURED CODE WRITTEN.
+
+
+enum ChaiType{
+    MASALA = "masala",
+    GINGER = "ginger"
+}
+
+function makeChai(type: ChaiType){
+    console.log(`Making ${type} Chai/Tea`);
+}
+
+makeChai(ChaiType.MASALA); // This is how we can only pass enum
+makeChai(ChaiType.GINGER);
+// makeChai("Masala"); // THIS WILL GENERATE ERROR
+
+
+// HETEROGENEOUS ENUMS(having different datatypes)
+
+// No issue but in INDUSTRY WE DON'T USE it.IT IS RECOMMENDED TO MAKE HOMOGENEOUS ENUMS.
+
+enum RandomVals{
+    _ID = 1,
+    NAME = "Chai"
+}
+// NOT RECOMMENDED TO USE coz we generally put same type of DATA in an enum
+
+// CONSTANT ENUMS
+
+const enum Sugar{
+    LOW = 1,
+    MEDIUM = 2,
+    HIGH = 3
+}
+
+let sugSpoon: Sugar = Sugar.MEDIUM;
+console.log(sugSpoon);
+
+
+
+
+// Enum   = Type-safe named constants with special TS support
+// Object = Plain JavaScript data structure
+
 
 
 
