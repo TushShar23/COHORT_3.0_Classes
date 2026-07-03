7faeef3c7751ea55c10bd8d09440b1dd8f12bc58
Tushar Sharma <onlyforcode23@gmail.com>
2026-07-03 20:20:05 +0100

    OOPS IN TS,SAME AS JS but method overloading can/cannot be implemented

--- Week-14-TypeScript/Lec-1/src/OOPS_TS.ts
+++ Week-14-TypeScript/Lec-1/src/OOPS_TS.ts
@@ -46,7 +46,8 @@ const val = c.reveal(); // c can't access this property directly but there is a 
 console.log(val);
 
 
-// PROTECTED
+// PROTECTED 
+// INHERITANCE in TS
 
 class Shop{
     protected shopName = "ChaiCorner";
@@ -140,11 +141,98 @@ class ModernChai{
         this._Sugar = value;
     }
 
+    // get and set are KEYWORDS.
+
 }
+// GETTERS and SETTERS are majorly used when we need to ACCESS VALUES which we can't DIRECTLY ACCESS.
 
 const mc1 = new ModernChai();
 mc1.Sugar = 4;
 console.log(mc1);
+
+
+
+
+// STATIC MEMBERS
+// STATIC MEMBERS are accessed using CLASSNAME.Static members are associated with THE CLASS NOT with the OBJECT OF the class.
+
+class StatExamp{
+    static shopName = "ChaiCoffee Corner";
+
+    constructor(public flavour: string){}
+}
+
+console.log(StatExamp.shopName);
+
+
+// COMPOSITION IN TS.
+
+class Heater{
+    heat():void{}
+}
+
+class MakeChai{
+    constructor(private heater: Heater){}
+
+    make(){
+        this.heater.heat()
+    }
+}
+
+// heater's type is HEATER(class).SO whereever we use this member "heater" we can use the method of that class(HEATER)
+
+
+// POLYMORPHISM
+// Polymorphism is the ability of the same method or function to perform different actions depending on the object or arguments.
+
+// 2 types 
+
+
+// 1. METHOD OVERRIDING
+class Animal{
+    speak(): void{
+        console.log("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal{
+    speak(): void{
+        console.log("Dog Barks!");
+    }
+}
+
+class Cat extends Animal{
+    speak(): void {
+        console.log("Cat Meows!");
+    }
+}
+
+const d = new Dog();
+d.speak();
+
+const C = new Cat();
+C.speak();
+//Method overriding occurs when a subclass provides its own specific implementation of a method that is already defined in its parent class.
+
+
+// 2. Method Overloading
+//JavaScript does not support true method overloading, but it can be simulated by handling different numbers or types of arguments within a single function
+
+class Calculator{
+    add(a: number, b:number): number{
+        if(b === undefined || a === undefined ){
+            return a + a;
+        }
+        return a + b;
+    }
+}
+
+const calc = new Calculator();
+// console.log(calc.add(2)); 
+
+// THIS IS NOT POSSIBLE IN TS AS we need to DEFINE THE TYPE OF OUR PARAMETERS.
+
+
 
 
 
